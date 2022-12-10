4880cc3f9c4acc80722b876c9c7991fd2c902a85
WowShiba <oopsdxm@gmail.com>
2022-12-10 09:34:40 +0800

    Update

--- app/application/controllers/app.rb
+++ app/application/controllers/app.rb
@@ -24,6 +24,29 @@ module Foodegrient
         result_response.to_json
       end
 
+      routing.on 'api/v1' do
+        routing.on 'menu' do
+          routing.is do
+            # POST /project/
+            routing.get do
+              
+              ori_keywords = routing.params['keywords']
+              result = Spoonacular::MenuMapper
+                     .new(App.config.FOOD_API_TOKEN)
+                     .search(ori_keywords.split('%20'))
+
+              message = result.recipes.to_s
+              result_response = Representer::HttpResponse.new(
+                Response::ApiResult.new(status: :ok, message: message) # rubocop:disable Style/HashSyntax
+              )
+
+              response.status = result_response.http_status_code
+              result_response.to_json
+              end
+            end
+          end
+      end
+
     end
   end
 end
