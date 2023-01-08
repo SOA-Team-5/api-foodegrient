37ac20ff31a13cfa9a1c4ea20754436928af21bb
Jason Hsieh <Jason.CY.Hsieh@ibm.com>
2023-01-08 19:09:33 +0800

    added: entity for drinks

---
+++ app/domain/menus/mappers/drink_mapper.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: false
+require_relative '../entities/drink'
+
+module Foodegrient
+  # Provides access to Food data
+  module Spoonacular
+    # Data Mapper: CocktailDb  -> Drink entity
+    class DrinkMapper
+      def initialize(data)
+        @data = data
+      end
+
+      def load_several()
+        @data.map do |item|
+          DrinkMapper.build_entity(item)
+        end
+      end
+
+      def self.build_entity(data)
+        DataMapper.new(data).build_entity
+      end
+
+      # Extracts entity specific elements from data structure
+      class DataMapper
+        def initialize(data)
+          @data = data
+          @recipe_db_table = $DB[:drink] # Create a dataset
+          @recipe_db_table.insert_conflict.multi_insert([{origin_id: id, image: image, name: title}]) unless @recipe_db_table.where(origin_id: id).count == 1
+        end
+
+        def build_entity
+          Entity::Drink.new(
+            id: id,
+            name: name, # rubocop:disable Style/HashSyntax
+            image: image, # rubocop:disable Style/HashSyntax
+          )
+        end
+
+        private
+        def id
+          @data['idDrink']
+        end
+
+        def image
+          @data['strDrinkThumb']
+        end
+
+        def title
+          @data['strDrink']
+        end
+
+      end
+    end
+  end
+end
